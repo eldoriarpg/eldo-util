fba9c7f7c1be0b57a84e3dcc47c2b37ab8095785
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-06-06 11:33:40 +0200

    Add consumer and Futures utils

---
+++ src/main/java/de/eldoria/eldoutilities/utils/Futures.java
@@ -0,0 +1,26 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) 2021 EldoriaRPG Team and Contributor
+ */
+
+package de.eldoria.eldoutilities.utils;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public final class Futures {
+    private Futures() {
+        throw new UnsupportedOperationException("This is a utility class.");
+    }
+
+    public static <T> BiConsumer<T, Throwable> whenComplete(Consumer<T> value, Consumer<Throwable> error) {
+        return (t, throwable) -> {
+            if (throwable != null) {
+                error.accept(throwable);
+                return;
+            }
+            value.accept(t);
+        };
+    }
+}
