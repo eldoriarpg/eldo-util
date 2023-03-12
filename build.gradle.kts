bea7a1d3b079468bc215abcd80d30eee2f0fda74
Lilly Tempest <46890129+rainbowdashlabs@users.noreply.github.com>
2023-03-12 14:35:09 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 group = "de.eldoria"
 var mainPackage = "eldoutilities"
 val shadebase = group as String? + "." + mainPackage + "."
-version = "1.14.3"
+version = "1.14.4"
 description = "Utility Library for spigot plugins used by the eldoria team."
 
 javaToolchains {
