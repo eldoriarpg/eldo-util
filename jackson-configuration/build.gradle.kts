8d6b552684b14a06983e1b32bde2c04f0095eb52
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-06-16 11:01:38 +0200

    Bump jackson-bukkit version

--- jackson-configuration/build.gradle.kts
+++ jackson-configuration/build.gradle.kts
@@ -1,4 +1,4 @@
 dependencies {
-    api("de.eldoria.jacksonbukkit:jackson-bukkit:1.1.0-SNAPSHOT")
+    api("de.eldoria.jacksonbukkit:jackson-bukkit:1.2.0")
     api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
 }
