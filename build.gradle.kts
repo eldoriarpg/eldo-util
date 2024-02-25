fd1a7a25453ee75763b08d28cf3e774a4dc2d245
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-02-25 12:10:12 +0100

    Bump version to 2.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 publishData {
     addRepo(Repo.main("","", false))
     addRepo(Repo.snapshot("SNAPSHOT","", false))
-    publishingVersion = "2.0.3"
+    publishingVersion = "2.0.4"
 }
 version = publishData.getVersion()
 
