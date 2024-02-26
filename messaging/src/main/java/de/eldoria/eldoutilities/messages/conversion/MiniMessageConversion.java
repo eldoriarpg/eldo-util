45677cc21a6da809f801af3f984c7ba45022c37e
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-02-26 14:22:06 +0100

    Apply license header

--- messaging/src/main/java/de/eldoria/eldoutilities/messages/conversion/MiniMessageConversion.java
+++ messaging/src/main/java/de/eldoria/eldoutilities/messages/conversion/MiniMessageConversion.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ *     Copyright (C) EldoriaRPG Team and Contributor
+ */
+
 package de.eldoria.eldoutilities.messages.conversion;
 
 import java.util.ArrayList;
