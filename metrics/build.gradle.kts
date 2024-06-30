4b49b084a35315d116a322e723cb400145296b2f
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-06-30 09:59:33 +0200

    Remove metrics inclusion and check enabled state without loading classes

--- metrics/build.gradle.kts
+++ metrics/build.gradle.kts
@@ -1,6 +1,5 @@
 description = "Wrapper for bstats"
 
 dependencies {
-    api("org.bstats", "bstats-bukkit", "3.0.2")
     compileOnly(libs.spigot)
 }
--- metrics/src/main/java/de/eldoria/eldoutilities/metrics/EldoMetrics.java
+++ metrics/src/main/java/de/eldoria/eldoutilities/metrics/EldoMetrics.java
@@ -6,39 +6,25 @@
 
 package de.eldoria.eldoutilities.metrics;
 
-import org.bstats.bukkit.Metrics;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 
 /**
- * Wrapper for the Metrics class of bstats. Just because they removed this shitty isEnabled() function.
+ * Helper for the Metrics class of bstats. Just because they removed this shitty isEnabled() function.
  */
-public class EldoMetrics extends Metrics {
-    private final boolean enabled;
-
-    /**
-     * Creates a new Metrics instance.
-     *
-     * @param plugin    Your plugin instance.
-     * @param serviceId The id of the service.
-     */
-    public EldoMetrics(JavaPlugin plugin, int serviceId) {
-        super(plugin, serviceId);
-        var bStatsFolder = new File(plugin.getDataFolder().getParentFile(), "bStats");
-        var configFile = new File(bStatsFolder, "config.yml");
-        var config = YamlConfiguration.loadConfiguration(configFile);
-        enabled = config.getBoolean("enabled", true);
-    }
+public class EldoMetrics {
 
     /**
      * Check if metrics are enabled
      *
      * @return true if enabled
      */
-    public boolean isEnabled() {
-        return enabled;
+    public static boolean isEnabled(Plugin plugin) {
+        var bStatsFolder = new File(plugin.getDataFolder().getParentFile(), "bStats");
+        var configFile = new File(bStatsFolder, "config.yml");
+        var config = YamlConfiguration.loadConfiguration(configFile);
+        return config.getBoolean("enabled", true);
     }
 }
